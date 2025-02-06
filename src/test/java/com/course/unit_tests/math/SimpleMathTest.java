6ee78639100ce88f3331254296a0475a77dce430
Thiago <thiagodeboliveira@outlook.com>
2025-02-06 14:15:51 -0300

    creating basic operations tests

--- src/test/java/com/course/unit_tests/math/SimpleMathTest.java
+++ src/test/java/com/course/unit_tests/math/SimpleMathTest.java
@@ -5,10 +5,47 @@ import org.junit.jupiter.api.Test;
 
 class  SimpleMathTest {
     @Test
-     voi d testSum(){
+     void testSum(){
         Simplemath math = new Simplemath();
+        Double firstNumber = 6.2D;
+        Double secondNumber = 2D;
         Double actual = math.sum(6.2D, 2D);
         Double expected = 8.2D;
-        assertEquals(expected, actual, "6.2 + 2 did not produce 8.2!");
+        assertEquals(expected, actual,
+                () -> firstNumber + "+" + secondNumber + " should be " + expected);
+    }
+
+    @Test
+    void testSub(){
+        Simplemath math = new Simplemath();
+        Double firstNumber = 6.2D;
+        Double secondNumber = 2D;
+        Double actual = math.subtraction(6.2D, 2D);
+        Double expected = 4.2D;
+        assertEquals(expected, actual,
+                () -> firstNumber + "-" + secondNumber + " should be " + expected);
+
+    }
+
+    @Test
+    void testMulti(){
+        Simplemath math = new Simplemath();
+        Double firstNumber = 6.2D;
+        Double secondNumber = 2D;
+        Double actual = math.multiplication(6.2D, 2D);
+        Double expected = 12.4D;
+        assertEquals(expected, actual,
+                () -> firstNumber + "*" + secondNumber + " should be " + expected);
+    }
+
+    @Test
+    void testDiv(){
+        Simplemath math = new Simplemath();
+        Double firstNumber = 6.2D;
+        Double secondNumber = 2D;
+        Double actual = math.division(6.2D, 2D);
+        Double expected = 3.1D;
+        assertEquals(expected, actual,
+                () -> firstNumber + "/" + secondNumber + " should be " + expected);
     }
 }
