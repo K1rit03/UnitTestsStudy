55684826706a68d7ec63fd04e9e21926ab2b0825
Thiago <thiagodeboliveira@outlook.com>
2025-02-07 13:47:39 -0300

    adding display name at the test methods

--- src/test/java/com/course/unit_tests/math/SimpleMathTest.java
+++ src/test/java/com/course/unit_tests/math/SimpleMathTest.java
@@ -1,11 +1,16 @@
 package com.course.unit_tests.math;
 
 import static org.junit.jupiter.api.Assertions.* ;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class  SimpleMathTest {
+@DisplayName("Test Math Operations in Simplemath Class ")
+class SimpleMathTest {
+
     @Test
-     void testSum(){
+    @DisplayName("Test 6.2 + 2 = 8.2")
+    void testSum(){
         Simplemath math = new Simplemath();
         Double firstNumber = 6.2D;
         Double secondNumber = 2D;
@@ -16,6 +21,7 @@ class  SimpleMathTest {
     }
 
     @Test
+    @DisplayName("Test 6.2 - 2 = 4.2")
     void testSub(){
         Simplemath math = new Simplemath();
         Double firstNumber = 6.2D;
@@ -24,10 +30,10 @@ class  SimpleMathTest {
         Double expected = 4.2D;
         assertEquals(expected, actual,
                 () -> firstNumber + "-" + secondNumber + " should be " + expected);
-
     }
 
     @Test
+    @DisplayName("Test 6.2 * 2 = 12.4")
     void testMulti(){
         Simplemath math = new Simplemath();
         Double firstNumber = 6.2D;
@@ -39,6 +45,7 @@ class  SimpleMathTest {
     }
 
     @Test
+    @DisplayName("Test 6.2 / 2 = 3.1")
     void testDiv(){
         Simplemath math = new Simplemath();
         Double firstNumber = 6.2D;
@@ -50,16 +57,28 @@ class  SimpleMathTest {
     }
 
     @Test
-    void testSquareRoot(){
+    @DisplayName("Test Division By Zero")
+    void testDivByZero(){
         Simplemath math = new Simplemath();
-        Double firstNumber = 4D;
-        Double actual = math.squareRoot(4D);
-        Double expected = 2D;
-        assertEquals(expected, actual,
-                () -> "The square root of " + firstNumber + " should be " + expected);
+        Double firstNumber = 6.2D;
+        Double secondNumber = 0D;
+        assertThrows(ArithmeticException.class, () -> math.division(6.2D, 0D),
+                () -> firstNumber + "/" + secondNumber + " should throw an ArithmeticException");
     }
 
     @Test
+    @DisplayName("Test Square Root of 4 = 2")
+    void testSquareRoot(){
+        Simplemath math = new Simplemath();
+        Double number = 4D;
+        Double actual = math.squareRoot(4D);
+        Double expected = 2D;
+        assertEquals(expected, actual,
+                () -> "The square root of " + number + " should be " + expected);
+    }
+
+    @Test
+    @DisplayName("Test Mean of 6.2 and 2 = 4.1")
     void testMean(){
         Simplemath math = new Simplemath();
         Double firstNumber = 6.2D;
@@ -70,4 +89,3 @@ class  SimpleMathTest {
                 () -> "The mean of " + firstNumber + " and " + secondNumber + " should be " + expected);
     }
 }
-
